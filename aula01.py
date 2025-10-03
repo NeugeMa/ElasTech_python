80f6415f3e69c5959ca77a5c7ee907371a4a816e
NeugeMa <mariana.n.dourado@gmail.com>
2025-10-03 11:21:41 -0300

    Aula01 - feita

---
+++ aula01.py
@@ -0,0 +1,6 @@
+# print - utilizado para a saída de informações
+print('Olá Mundo!')
+print('Seja bem-vinda ao curso de Introdução ao Python')
+
+# input - utilizado para entrada de informações 
+input('Qual a linguagem de programação que você está estudando? ')
