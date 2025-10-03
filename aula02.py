481391ff2a9f8450264de3497896e5f9eef239d4
NeugeMa <mariana.n.dourado@gmail.com>
2025-10-03 16:39:39 -0300

    Aula02 - feita

---
+++ aula02.py
@@ -0,0 +1,26 @@
+# Variáveis
+print('O que são variáveis?')
+print('São espaços na mémoria, que por ex: Temos um guarda-roupa e dentro dele colocamos roupas, sapatos e (etc).')
+print('E sempre que quiser você pode acessar ela.')
+# Toda variável recebe um NOME - Uma forma de identificação 
+
+# Criando uma variável 
+idade = 20  # Idade RECEBE 20
+nome = 'Mariana'
+print(idade, nome)
+
+# Tipos de Variáveis 
+''' 1. int - números inteiros
+    2. float - números decimais
+    3. str - textos (string)
+    4. bool - booleano (True ou False)  '''
+
+idade = 20          # int 
+altura = 1.60       # float 
+nome = 'Mariana'    # str
+estudando = True    # bool
+
+# Objetivo: Obter um dado do usuário e armazenar em uma variável
+cor = input('Qual sua cor favorita? ')
+print('Sua cor favorita é', cor) # Podemos imprimir varias variáveis utilizando vírgula
+
