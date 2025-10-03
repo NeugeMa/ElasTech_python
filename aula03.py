c4cff280b11df35a85dba7ff2b539024c10a0c1b
NeugeMa <mariana.n.dourado@gmail.com>
2025-10-03 19:56:03 -0300

    Aula03 - feita

---
+++ aula03.py
@@ -0,0 +1,39 @@
+# Operações Aritméticas 
+''' 1. Soma: +
+    2. Subtração: - 
+    3. Multiplicação: * 
+    4. Divisão: / 
+    
+    5. Divisão Inteira: //
+    6. Resto da divisão: %
+    7. Potência: **    '''
+
+num1 = 10
+num2 = 5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+
+# Booleanos 
+idade1 = 20
+idade2 = 21
+
+print(idade1 > idade2)  # Verificando se idade1 é maior que idade2 = FALSE
+print(idade1 <= idade2) # Verificando se idade1 é menor/igual que idade 2 = TRUE
+
+print('Mari' == 'mari') # Fazendo uma comparação de texto, se eles são "iguais" = FALSE
+
+
+altura1 = 1.70
+altura2 = 1.60
+altura3 = altura2
+
+print(altura1 >= altura2) # Verificando se altura1 é maior/igual que altura2 = TRUE
+print(altura2 > altura3)  # Verificando se altura2 é maior que altura3 = FALSE 
