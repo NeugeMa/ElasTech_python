2b2c5f2c51b936bf19c564bf608d6398b9667293
NeugeMa <mariana.n.dourado@gmail.com>
2025-10-06 16:42:45 -0300

    Aula10 - feita

---
+++ Modulo03/aula10.py
@@ -0,0 +1,16 @@
+# Dicionários
+
+dicionario = {}
+dicionario = dict()
+
+dicionario = { 'nome': 'Mariana', 'idade': 20, 'altura': 1.62}
+
+# Acessando valores
+print(dicionario)
+print(dicionario['nome'])
+print(dicionario['idade'])
+print(dicionario['altura'])
+
+# Adicionando valores
+dicionario['Python']= True
+print(dicionario)
